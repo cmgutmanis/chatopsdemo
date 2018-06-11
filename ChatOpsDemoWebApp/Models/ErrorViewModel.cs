7f29440b66bd718a2a65ccb95b151868d1f2c2b9
First Last <christopher.gutmanis@aurora.org>
2018-06-10 21:58:20 -0500

    initial commit

---
+++ ChatOpsDemoWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChatOpsDemoWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
